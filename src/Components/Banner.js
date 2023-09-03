8eed7dc5735dffbf1712929b0023806c3a573f9f
ManasKumarLal <skillszone100@gmail.com>
2023-09-04 03:32:06 +0530

    responsive

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,13 +3,13 @@ import { TypeAnimation } from "react-type-animation";
 export default function Banner() {
   return (
     <div className="selection:bg-black selection:text-[red]">
-      <div className="bg-[#2699fb] w-full h-[80vh] pt-[180px] pb-[240px] relative">
-        <div className="max-w-[1240px] mx-auto text-center font-bold my-[30px]">
+      <div className="bg-[#2699fb] w-full h-[80vh] pt-[40px] pb-[240px] relative">
+        <div className="max-w-[1240px] mx-auto text-center font-bold">
           <div className="md:text-3xl text-2xl">Learn with us</div>
           <div className="text-white md:text-[70px] text-[50px] mt-10">
             Grow with us.
           </div>
-          <div className="px-[5%] md:px-[10%] lg:px-[20%] md:text-[50px] text-[40px] text-white mt-16 md:mt-5">
+          <div className="px-[1%] md:px-[10%] lg:px-[20%] md:text-[50px] text-[40px] text-white md:mt-5">
             Learn{" "}
             <TypeAnimation
               sequence={[
@@ -27,7 +27,7 @@ export default function Banner() {
               repeat={Infinity}
             />
           </div>
-          <button className="w-[140px] bg-black text-white rounded py-3 mt-5 px-5 absolute bottom-[80px] -translate-x-[50%]">Get Started</button>
+          <button className="w-[140px] bg-black text-white rounded py-3 mt-5 px-5 absolute bottom-[60px] -translate-x-[50%]">Get Started</button>
         </div>
       </div>
     </div>
